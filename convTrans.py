9ccb46a0bd4eb02611b86366d0d05d2aa994198e
wangchuxuan <623170681@qq.com>
2022-03-02 15:49:11 +0800

    correct the attn @ v process

--- convTrans.py
+++ convTrans.py
@@ -1,22 +1,40 @@
 import torch
 import torch.nn as nn
-
+from torch.nn import functional as F
 
 class convAttention(nn.Module):
-    def __init__(self, winSize=7, dim=96, num_heads=3, attn_drop=0.):
+    def __init__(self, win_size=(7, 7), dim=96, num_heads=3, attn_drop=0.):
         super(convAttention, self).__init__()
         self.num_heads = num_heads
         self.qkv = torch.nn.Conv2d(dim, dim * 3, kernel_size=(1, 1), stride=(1, 1), padding=(0, 0))
-        self.windowSize = winSize
-        self.convAttention = torch.nn.Conv2d(dim // num_heads * 2, winSize * winSize, kernel_size=(winSize, winSize),
-                                             stride=1, padding=(winSize // 2, winSize // 2))
+        self.window_size = win_size
+        self.convAttention = torch.nn.Conv2d(dim // num_heads * 2, win_size[0] * win_size[1], kernel_size=win_size,
+                                             stride=1, padding=(win_size[0] // 2, win_size[1] // 2))
         self.softmax = nn.Softmax(dim=1)
         self.scale = (dim // num_heads) ** -0.5
 
+        # position encoding
+        self.relative_position_bias_table = nn.Parameter(
+            torch.zeros((2 * win_size[0] - 1) * (2 * win_size[1] - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+
+        self.v_padding = nn.ConstantPad2d((win_size[1] // 2, win_size[1] // 2, win_size[0] // 2, win_size[0] // 2), 0)
+
+        # TODO: position encoding and rewrite the for loop for v mat generation
+        coords_h = torch.arange(self.window_size[0])
+        coords_w = torch.arange(self.window_size[1])
+        coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
+        coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
+        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
+        relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
+        relative_coords[:, :, 0] += self.window_size[0] - 1  # shift to start from 0
+        relative_coords[:, :, 1] += self.window_size[1] - 1
+        relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
+        relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
+        self.register_buffer("relative_position_index", relative_position_index)
+
         self.proj = torch.nn.Conv2d(dim, dim, kernel_size=(1, 1), stride=(1, 1), padding=(0, 0))
         self.proj_drop = torch.nn.Dropout2d(p=attn_drop)
-
-        # TODO: position encoding
+        # self.register_buffer()
 
     def forward(self, x):
         B, C, H, W = x.shape
@@ -24,20 +42,40 @@ class convAttention(nn.Module):
         q, k, v = qkv[0], qkv[1], qkv[2]  # Batch, num_heads, dim//num_heads, H, W
         attentionMap = self.convAttention(
             torch.cat(
-                (torch.flatten(q * self.scale, start_dim=0, end_dim=1), torch.flatten(q, start_dim=0, end_dim=1)), dim=1
+                (torch.flatten(q * self.scale, start_dim=0, end_dim=1), torch.flatten(k, start_dim=0, end_dim=1)), dim=1
             )
         )
-        attentionMap = self.softmax(attentionMap).reshape(B, self.num_heads, self.windowSize ** 2, H, W).unsqueeze(2)
+        relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)]
+        attentionMap = self.softmax(attentionMap).reshape(B, self.num_heads, self.window_size[0] * self.window_size[1], H, W)
         # Batch, num_heads, w*w, H, W
 
-        x = torch.sum(v.unsqueeze(3) * attentionMap, dim=3).reshape(B, C, H, W)
+        v_padding = self.v_padding(
+            v.flatten(0, 1)
+        )
+        v_padding = v_padding.view(B, self.num_heads, C // self.num_heads,
+                                   H + self.window_size[0] - 1, W + self.window_size[1] - 1).unsqueeze(3)
+        # Batch, num_heads, dim//num_heads, 1, Hp, Wp
+
+        v = torch.cat(
+            [v_padding[:, :, :, :, i // self.window_size[0]:i // self.window_size[0] + H,
+             i % self.window_size[0]:i % self.window_size[0] + H]
+             for i in range(self.window_size[0] * self.window_size[1])
+             ], dim=3
+        )
+
+        x = torch.sum(v * attentionMap.unsqueeze(2), dim=3).reshape(B, C, H, W)
+        # print(torch.max(x - v.reshape(B, C, H, W)))
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
 
 
 if __name__ == '__main__':
-    at = convAttention()
-    a = torch.rand(8, 96, 24, 24)
-    b = at(a)
-    print(b.shape)
+    from torchsummary import summary
+    at = convAttention().cuda()
+    a = torch.rand(8, 96, 24, 24).cuda()
+    summary(at, (96, 48, 48), batch_size=8)
+    # b = at(a)
+    # print(b.shape)
+
+
--- main.py
+++ main.py
@@ -1,17 +1,66 @@
-# This is a sample Python script.
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import torchvision.transforms as t
+# h = 2
+# w = 3
+#
+# coords_h = torch.arange(h)
+# coords_w = torch.arange(w)
+# coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
+# print('coords:\n', coords)
+# coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
+# print('coords_flatten:\n', coords_flatten)
+# relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
+# relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
+# print('relative coords:\n', relative_coords, '\n', relative_coords[:, :, 0])
+# # relative_coords[:, :, 0] += h - 1  # shift to start from 0
+# # relative_coords[:, :, 1] += w - 1
+# relative_coords[:, :, 0] *= 2 * w - 1
+# print('relative coords:\n', relative_coords)
+# print(relative_coords[:, :, 0], relative_coords[:, :, 1])
+# relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
+# print('relative position index:\n', relative_position_index.view(-1))
+# a = torch.nn.Conv2d(5, 10, 3)
+# b = torch.rand(2, 1, 5, 10, 10)
+# print(a(b))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# image = torch.arange(25).view(5, 5).unsqueeze(0).unsqueeze(0).repeat((9, 1, 1, 1)).float()
+# image.index_select
+# image = image.flatten(0, 1)
+# print(image.shape)
+# coords_h = torch.arange(3)
+# coords_w = torch.arange(3)
+#
+# coords = torch.stack(torch.meshgrid([coords_h, coords_w])).view(2, -1).unsqueeze(2).permute(1, 0, 2)  # 2, Wh, Ww
+# print(coords)
+# coords[:, 0, :] = -1
+# coords[:, 1, :] = 0
+# trans_mat = torch.cat(
+#             [
+#                 torch.eye(2).repeat((9, 1, 1)),
+#                 coords
+#             ],
+#             dim=2
+#             )
+# grid = F.affine_grid(trans_mat, image.shape, align_corners=False)
+# img = F.grid_sample(image, grid, align_corners=False)
+# print(img)
+# a = t.functional.affine(img)
+img = torch.arange(25).view(5, 5).unsqueeze(0).unsqueeze(0).repeat(1, 1, 1, 1)
+v_padding = nn.ConstantPad2d(1, 0)
+img_p = v_padding(img)
+# img = torch.roll(img_p, )
+# print(img_p)
+# print(img_p.size())
+# print(torch.argmax(img_p))
+print(img_p)
+a = torch.cat(
+            [img_p[:, :, i//3:i//3+3, (i % 3):((i % 3)+3)]
+             for i in range(9)
+             ], dim=1
+        )
+print(a)
+# img = torch.take(img_p, torch.tensor([[1, 2, 3], [10, 12, 18]]))
+# print(img)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    # wtf?
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
