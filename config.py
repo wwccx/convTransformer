d18e1ffb0310fafdf5458128ce42acba9f36943f
wangchuxuan <623170681@qq.com>
2022-07-14 20:41:47 +0800

    I forgot the two new files.. Dame..

---
+++ config.py
@@ -0,0 +1,64 @@
+import os
+import yaml
+from yacs.config import CfgNode as CN
+
+_C = CN()
+
+_C.DATA = CN()
+_C.DATA.BATCH_SIZE = 128
+_C.DATA.DATASET = 'grasp'
+_C.DATA.MIXUP_ON = False
+_C.DATA.IMG_SIZE = (1, 96, 96)
+
+_C.DATA.MIXUP = CN()
+_C.DATA.MIXUP.MIXUP_ALPHA = 0.8
+_C.DATA.MIXUP.CUTMIX_ALPHA = 1.0
+_C.DATA.MIXUP.CUTMIX_MINMAX = None
+_C.DATA.MIXUP.PROB = 1.0
+_C.DATA.MIXUP.SWITCH_PROB = 0.5
+_C.DATA.MIXUP.MODE = 'batch'
+_C.DATA.MIXUP.LABEL_SMOOTHING = 0.1
+_C.DATA.MIXUP.NUM_CLASSES = 2
+
+_C.MODEL = CN()
+_C.MODEL.ARCH = 'convTrans'
+_C.MODEL.NUM_CLASSES = 32
+_C.MODEL.IN_CHANNELS = 1
+_C.MODEL.EMBED_DIM = 96
+_C.MODEL.PATCH_EMBED_SIZE = (4, 4)
+_C.MODEL.PATCH_MERGE_SIZE = (2, 2)
+_C.MODEL.FULLY_CONV_FOR_GRASP = True
+_C.MODEL.WINDOW_SIZE = (7, 7)
+_C.MODEL.NUM_HEADS = (3, 12)
+_C.MODEL.DEPTHS = (2, 6)
+_C.MODEL.DROP_PATH_RATE = 0.1
+
+_C.TRAIN = CN()
+_C.TRAIN.EPOCHS = 100
+_C.WARMUP_EPOCHS = 0
+_C.TRAIN.BASE_LR = 5e-4
+_C.TRAIN.MIN_LR = 5e-6
+_C.TRAIN.WARMUP_LR = 5e-7
+_C.TRAIN.AMP_LEVEL = 'O1'
+_C.TRAIN.CLIP_GRAD = 0
+
+_C.TRAIN.LR_SCHEDULE = CN()
+_C.TRAIN.LR_SCHEDULE.TYPE = 'cosine'
+
+_C.TRAIN.OPTIMIZER = CN()
+_C.TRAIN.OPTIMIZER.TYPE = 'adamw'
+_C.TRAIN.OPTIMIZER.WEIGHT_DECAY = 0.05
+_C.TRAIN.OPTIMIZER.BETA = (0.9, 0.999)
+_C.TRAIN.OPTIMIZER.EPS = 1e-8
+
+
+def update_config(opt):
+    cfg = _C.clone()
+    cfg.TRAIN.EPOCHS = opt.n_epochs
+    cfg.DATA.BATCH_SIZE = opt.batch_size
+    cfg.MODEL.ARCH = opt.model
+    cfg.DATA.DATASET = opt.dataset
+    cfg.TRAIN.AMP_LEVEL = opt.amp_level
+    cfg.DATA.MIXUP_ON = opt.mixup
+    cfg.freeze()
+    return cfg
---
+++ load_model.py
@@ -0,0 +1,40 @@
+from convTrans import convTransformer
+
+
+def build_model(config):
+    if config.MODEL.ARCH == 'convTrans':
+        model = convTransformer(
+            in_chans=config.MODEL.IN_CHANNELS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            embed_dim=config.MODEL.EMBED_DIM,
+            depths=config.MODEL.DEPTHS,
+            num_heads=config.MODEL.NUM_HEADS,
+            patch_embedding_size=config.MODEL.PATCH_EMBED_SIZE,
+            patch_merging_size=config.MODEL.PATCH_MERGE_SIZE,
+            window_size=config.MODEL.WINDOW_SIZE,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            fully_conv_for_grasp=config.MODEL.FULLY_CONV_FOR_GRASP
+        )
+
+    else:
+        raise NotImplementedError(f"Unkown model: {config.MODEL.ARCH}")
+    return model
+    # if 'convTrans' in opt.model:
+    #     if 'vib' in opt.dataset:
+    #         self.network = convTransformer(num_classes=2, in_chans=1, window_size=(1, 3),
+    #                                        patch_embedding_size=(1, 32), patch_merging_size=(1, 2),
+    #                                        ).to(self.device)
+    #     elif 'grasp' in opt.dataset:
+    #         self.network = convTransformer(in_chans=1, num_classes=32,
+    #                                        embed_dim=96, depths=(2, 6),
+    #                                        num_heads=(3, 12),
+    #                                        patch_embedding_size=(4, 4),
+    #                                        fully_conv_for_grasp=True).to(self.device)
+    #
+    #     else:
+
+
+
+
+
+    return model
